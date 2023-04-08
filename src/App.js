502b0bdc16f5c6b986e2f9d46baf479db95698e9
tony tolver <andrademaresantonio00@gmail.com>
2023-04-08 17:14:59 -0600

    Se agrego la barra de navegación

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, {Fragment} from "react";
 
 /*** Layout */
 import Header from "./components/layout/Header";
+import Navegacion from "./components/layout/Navegacion";
 
 function App(){
   return(
     <Fragment>
       <Header />
+      <div className="grid contenedor contenido-principal">
+        <Navegacion />
+        <main class="caja-contenido col-9">
+          {/* TODO: routing a los componentes */}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi aut optio soluta quos, molestiae voluptas rerum error, fuga tenetur esse consequatur. Sed ad eaque magni similique quo praesentium nesciunt dolorum!
+        </main>
+      </div>
+
     </Fragment>
   )
 }
